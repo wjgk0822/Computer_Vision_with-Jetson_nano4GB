ce247a6a1a7c33daf5293249e2c254e6fdda447a
wjgk0822 <wjgk0822@naver.com>
2023-11-24 10:26:03 +0900

    simple qr detector v4l2

--- cam_test.py
+++ cam_test.py
@@ -1,111 +1,116 @@
+#cam test
+
+from pyzbar import pyzbar
+import argparse
+import datetime
+import imutils
 import time
 import cv2
-import torch
-import numpy as np
+import os 	# so the script can automatically send the CSV file to dropbox
 
-from numpy import random
-from models.experimental import attempt_load
-from utils.datasets import letterbox
-from utils.general import check_img_size, check_requirements, non_max_suppression, scale_coords
-from utils.plots import plot_one_box
-from utils.torch_utils import select_device, time_synchronized
+# construct the argument parser and parse the arguments
+# ap = argparse.ArgumentParser()
+# ap.add_argument("-o", "--output", type=str, default="barcodes.csv",
+# 	help="path to output CSV file containing barcodes")
+# args = vars(ap.parse_args())
 
+# initialize the video stream and allow the camera sensor to warm up
+print("[INFO] starting video stream...")
+# vs = VideoStream(src=0).start()   
+cap = cv2.VideoCapture(1,cv2.CAP_V4L2)#VideoStream(usePiCamera=True).start() 
+time.sleep(2.0)
 
-WEIGHTS = 'yolov7-tiny.pt'
-IMG_SIZE = 640
-DEVICE = ''
-AUGMENT = False
-CONF_THRES = 0.25
-IOU_THRES = 0.45
-CLASSES = None
-AGNOSTIC_NMS = False
+cap.set(3,640)
+cap.set(4,480)
+cap.set(5,30)
 
+# open the output CSV file for writing and initialize the set of
+# barcodes found thus far
+csv = open("barcode.csv","w")
+found = set()
 
-# Webcam
-cap = cv2.VideoCapture(1,cv2.CAP_V4L2)
+# loop over the frames from the video stream
+while True:
+	# grab the frame from the threaded video stream and resize it to
+	# have a maximum width of 400 pixels
+	ret,frame = cap.read()
+	#frame = imutils.resize(frame, width=400)
+    # vs.set(3,640)
+    # vs.set(4,480)
+    # vs.set(5,30)
+    
+ 
 
-# Initialize
-device = select_device(DEVICE)
-half = device.type != 'cpu'  # half precision only supported on CUDA
-print('device:', device)
+	# find the barcodes in the frame and decode each of the barcodes
+	barcodes = pyzbar.decode(frame)
 
-# Load model
-model = attempt_load(WEIGHTS, map_location=device)  # load FP32 model
-stride = int(model.stride.max())  # model stride
-imgsz = check_img_size(IMG_SIZE, s=stride)  # check img_size
-if half:
-    model.half()  # to FP16
+	# loop over the detected barcodes
+	for barcode in barcodes:
+		# extract the bounding box location of the barcode and draw
+		# the bounding box surrounding the barcode on the image
+		(x, y, w, h) = barcode.rect
+		cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
 
-# Get names and colors
-names = model.module.names if hasattr(model, 'module') else model.names
-colors = [[random.randint(0, 255) for _ in range(3)] for _ in names]
+		# the barcode data is a bytes object so if we want to draw it
+		# on our output image we need to convert it to a string first
+		barcodeData = barcode.data.decode("utf-8")
+		barcodeType = barcode.type
 
-# Run inference
-if device.type != 'cpu':
-    model(torch.zeros(1, 3, imgsz, imgsz).to(device).type_as(next(model.parameters())))  # run once
+		# draw the barcode data and barcode type on the image
+		text = "{} ({})".format(barcodeData, barcodeType)
+		cv2.putText(frame, text, (x, y - 10),
+			cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
 
+		# if the barcode text is currently not in our CSV file, write
+		# the timestamp + barcode to disk and update the set
+		if barcodeData not in found:
+			csv.write("{},{}\n".format(datetime.datetime.now(),
+				barcodeData))
+			csv.flush()
+			found.add(barcodeData)
+			
 
-# Detect function
-def detect(frame):
-    # Load image
-    img0 = frame
+	# show the output frame
+	cv2.imshow("Barcode Scanner", frame)
+	key = cv2.waitKey(1) & 0xFF
+ 
+	# if the `q` key was pressed, break from the loop
+	if key == ord("q"):
+		break
 
-    # Padded resize
-    img = letterbox(img0, imgsz, stride=stride)[0]
-
-    # Convert
-    img = img[:, :, ::-1].transpose(2, 0, 1)  # BGR to RGB, to 3x416x416
-    img = np.ascontiguousarray(img)
-
-    img = torch.from_numpy(img).to(device)
-    img = img.half() if half else img.float()  # uint8 to fp16/32
-    img /= 255.0  # 0 - 255 to 0.0 - 1.0
-    if img.ndimension() == 3:
-        img = img.unsqueeze(0)
-
-
-    # Inference
-    t0 = time_synchronized()
-    pred = model(img, augment=AUGMENT)[0]
-
-    # Apply NMS
-    pred = non_max_suppression(pred, CONF_THRES, IOU_THRES, classes=CLASSES, agnostic=AGNOSTIC_NMS)
-
-    # Process detections
-    det = pred[0]
-
-    s = ''
-    s += '%gx%g ' % img.shape[2:]  # print string
-
-    if len(det):
-        # Rescale boxes from img_size to img0 size
-        det[:, :4] = scale_coords(img.shape[2:], det[:, :4], img0.shape).round()
-
-        # Print results
-        for c in det[:, -1].unique():
-            n = (det[:, -1] == c).sum()  # detections per class
-            s += f"{n} {names[int(c)]}{'s' * (n > 1)}, "  # add to string
-
-        # Write results
-        for *xyxy, conf, cls in reversed(det):
-            label = f'{names[int(cls)]} {conf:.2f}'
-            plot_one_box(xyxy, img0, label=label, color=colors[int(cls)], line_thickness=3)
-
-        print(f'Inferencing and Processing Done. ({time.time() - t0:.3f}s)')
-
-    # return results
-    return img0
+# close the output CSV file do a bit of cleanup
+print("[INFO] cleaning up...")
+csv.close()
+#cv2.destroyAllWindows()
+cap.release()
+cv2.destroyAllWindows()
 
 
 
+# import cv2
 
-# main
-check_requirements(exclude=('pycocotools', 'thop'))
-with torch.no_grad():
-    while(True):
-        ret, frame = cap.read()
-        result = detect(frame)
-        cv2.imshow('pred_image', result)
+# camera_id = 1
+# delay = 1
+# window_name = 'OpenCV Barcode'
 
-        if cv2.waitKey(1) == ord('q'):
-            break
+# bd = cv2.barcode.BarcodeDetector()
+# cap = cv2.VideoCapture(camera_id,cv2.CAP_V4L2)
+
+# while True:
+#     ret, frame = cap.read()
+
+#     if ret:
+#         ret_bc, decoded_info, _, points = bd.detectAndDecode(frame)
+#         if ret_bc:
+#             frame = cv2.polylines(frame, points.astype(int), True, (0, 255, 0), 3)
+#             for s, p in zip(decoded_info, points):
+#                 if s:
+#                     print(s)
+#                     frame = cv2.putText(frame, s, p[1].astype(int),
+#                                         cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 2, cv2.LINE_AA)
+#         cv2.imshow(window_name, frame)
+
+#     if cv2.waitKey(delay) & 0xFF == ord('q'):
+#         break
+
+# cv2.destroyWindow(window_name)
