925c9b977f4f776f2c06db01b3af63c720400d62
wjgk0822 <wjgk0822@naver.com>
2023-11-29 10:47:14 +0900

    Add meanshift+camshift

--- test20.py
+++ test20.py
@@ -195,8 +195,61 @@ def meanShift():
 
     cap.release()
     cv2.destroyAllWindows()
+    
+def camShift():
+    global frame,frame2,inputmode,trackWindow,roi_hist
 
-meanShift()
+    try:
+        # 저장된 영상 불러옴
+        cap = cv2.VideoCapture(1,cv2.CAP_V4L2)
+        cap.set(3,640)
+        cap.set(4,480)
+    except Exception as e:
+        print(e)
+        return
+
+    ret, frame = cap.read()
+
+    cv2.namedWindow('frame')
+    cv2.setMouseCallback('frame',onMouse,param=(frame,frame2))
+
+    # meanShift 함수의 3번째 인자. 10회 반복 혹은 C1_o ~ C1_r의 차이가 1pt 날 때까지 작동
+    termination = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10,1)
+
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
+        if trackWindow is not None:
+            hsv = cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
+            dst = cv2.calcBackProject([hsv],[0],roi_hist,[0,180],1)
+            ret, trackWindow = cv2.CamShift(dst,trackWindow,termination)
+
+            ### 이 부분 수정
+            pts = cv2.boxPoints(ret)
+            pts = np.int0(pts)
+            cv2.polylines(frame,[pts],True,(0,255,0),2)
+
+        cv2.imshow('frame',frame)
+
+        k = cv2.waitKey(60) & 0xFF
+        if k == 27:
+            break
+        # i를 눌러서 영상을 멈춰서 roi 설정    
+        if k == ord('i'):
+            print("Meanshift를 위한 지역을 선택하고 키를 입력해라")
+            inputmode = True
+            frame2 = frame.copy()
+
+            while inputmode:
+                cv2.imshow('frame',frame)
+                cv2.waitKey(0)
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+#meanShift()
+camShift()
 #출처: https://leechamin.tistory.com/334 [참신러닝 (Fresh-Learning):티스토리]
                 
             
